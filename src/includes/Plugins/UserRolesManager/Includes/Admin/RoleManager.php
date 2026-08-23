2156be7428a874bc8fe47dd6db69c598a976bd20
MrTrilB <anthony.bluthner@outlook.com>
2026-08-23 10:43:09 +0100

    feat: update capability info button to improve accessibility and styling

--- src/includes/Plugins/UserRolesManager/Includes/Admin/RoleManager.php
+++ src/includes/Plugins/UserRolesManager/Includes/Admin/RoleManager.php
@@ -223,12 +223,9 @@ final class RoleManager extends Manager {
 											<div class="wikipress-capability-title d-flex align-items-center justify-content-center gap-1">
 												<h6 class="card-title mb-0 text-center"><?php echo esc_html( $description ?: $capability ); ?></h6>
 												<?php if ( $wordpress_description ) : ?>
-													<a href="#" class="btn btn-primary wikipress-capability-info p-1" data-bs-toggle="tooltip" data-bs-placement="top" title="<?php echo esc_attr( $wordpress_description ); ?>" aria-label="<?php esc_attr_e( 'Capability information', 'wikipress' ); ?>">
-														<span class="fa-stack fa-2x">
-															<i class="fa-solid fa-circle fa-stack-2x"></i>
-															<i class="fa-solid fa-circle-info fa-stack-1x" aria-hidden="true"></i>
-														</span>
-													</a>
+													<button type="button" class="btn btn-link p-0 align-baseline .wikipress-capability-info" data-bs-toggle="tooltip" data-bs-placement="top" title="<?php echo esc_attr( $wordpress_description ); ?>" aria-label="<?php esc_attr_e( 'Capability information', 'wikipress' ); ?>">
+														<i class="fa-solid fa-info" aria-hidden="true"></i>
+													</button>
 												<?php endif; ?>
 											</div>
 											<input class="btn-check" type="checkbox" name="capabilities[]" value="<?php echo esc_attr( $capability ); ?>" id="<?php echo esc_attr( $input_id ); ?>" autocomplete="off"<?php checked( $is_selected ); ?> />
